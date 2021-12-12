0e5c271cb9dc86bfede41f389ef05ab884c5c32f
Estefane21 <lufloresfany@gmail.com>
2021-12-12 16:27:04 -0400

    Atualizando o italico

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+
 import api from "../../Api";
 import HeaderResponsive from "../../Components/HeaderResponsive";
 import "./styles.css";
